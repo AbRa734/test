a50f2bd88cfb56feb2452b6b3d2f29e8c812c234
Oliwier Szafrański <oliwierszafranski@MacBook-Air-Oliwier.local>
2022-09-01 15:57:12 +0200

    v3.1 + v.4 repaired

--- code.py
+++ code.py
@@ -1,14 +1,10 @@
 print("Hello, Git!")
 print("Test")
 for _ in range(10):
-<<<<<<< HEAD
     print("Hello!")
 
 testowa_tablica = ['osoba1']
-=======
-  print("Hello!")
 
   
   
 print('aaa')
->>>>>>> 945d77b2de0ebdc1ffe11cf335f2b8120e650574
